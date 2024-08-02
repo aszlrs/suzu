5a7cb16fae51f3b3222d0c62ea25e766cd63eaec
apz <adrpzal@outlook.com>
2024-08-02 17:18:49 -0600

    ihui

--- Vistas/principal.php
+++ Vistas/principal.php
@@ -20,8 +20,6 @@
         <div class="grid-item">2</div>
         <div class="grid-item">3</div>
         <div class="grid-item">4</div>
-        <div class="grid-item">5</div>
-        <div class="grid-item">6</div>
     </div>
 
 
